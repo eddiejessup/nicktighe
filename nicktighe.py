eb27be90514d27e71163d2b766f6acc68f1dc003
Elliot Marsden <elliot.marsden@gmail.com>
2014-12-28 23:13:42 +0000

    Rewrite app to work with Frozen Flask

---
+++ nicktighe.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from flask import Flask, render_template
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_GB')
+
+app = Flask(__name__)
+
+
+@app.route('/index.html')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/gallery.html')
+def gallery():
+    return render_template('gallery.html')
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)

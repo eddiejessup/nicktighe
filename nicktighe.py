ab2e209a1b47cc77256751571109ce068cb76d83
Elliot Marsden <elliot.marsden@gmail.com>
2015-01-08 14:57:52 +0000

    Move about section text to home page and remove about page

--- nicktighe.py
+++ nicktighe.py
@@ -12,11 +12,6 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/about.html')
-def about():
-    return render_template('about.html')
-
-
 @app.route('/contact.html')
 def contact():
     return render_template('contact.html')

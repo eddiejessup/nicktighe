d04fa18e68878eeb139a424efa1c07c6738532c1
Elliot Marsden <elliot.marsden@gmail.com>
2015-01-10 12:25:55 +0000

    Make gallery using a loop, and add captions

--- nicktighe.py
+++ nicktighe.py
@@ -7,6 +7,20 @@ locale.setlocale(locale.LC_ALL, locale.getdefaultlocale())
 app = Flask(__name__)
 
 
+items = [
+    {'filename': 'gallery/pub.jpeg',
+     'title': 'Scarborough Hotel. Oil on canvas, 44 cm by 62 cm'},
+    {'filename': 'gallery/cow.jpeg',
+     'title': 'For the Saward family. Oil on canvas, 42 cm by 59 cm'},
+    {'filename': 'gallery/dog.jpeg',
+     'title': 'Archie. Pencil and charcoal on paper, 21 cm by 29 cm'},
+    {'filename': 'gallery/owl.jpeg',
+     'title': 'Bowland Hen Harrier. Pencil on paper, 29 cm by 42 cm'},
+    {'filename': 'gallery/bluebell.png',
+     'title': 'Untitled. Ink, pencil, chalk, watercolour and acrylic.'},
+]
+
+
 @app.route('/index.html')
 def home():
     return render_template('home.html')
@@ -19,7 +33,7 @@ def contact():
 
 @app.route('/gallery.html')
 def gallery():
-    return render_template('gallery.html')
+    return render_template('gallery.html', items=items)
 
 
 # if __name__ == '__main__':

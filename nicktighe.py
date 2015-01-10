648e3cbdd1f87aa88f81dafcedf6639620bc505b
Elliot Marsden <elliot.marsden@gmail.com>
2015-01-10 12:49:51 +0000

    Add full stops to end of descriptions

--- nicktighe.py
+++ nicktighe.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 items = [
     {'filename': 'gallery/pub.jpeg',
-     'title': 'Scarborough Hotel. Oil on canvas, 44 cm by 62 cm'},
+     'title': 'Scarborough Hotel. Oil on canvas, 44 cm by 62 cm.'},
     {'filename': 'gallery/cow.jpeg',
-     'title': 'For the Saward family. Oil on canvas, 42 cm by 59 cm'},
+     'title': 'For the Saward family. Oil on canvas, 42 cm by 59 cm.'},
     {'filename': 'gallery/dog.jpeg',
-     'title': 'Archie. Pencil and charcoal on paper, 21 cm by 29 cm'},
+     'title': 'Archie. Pencil and charcoal on paper, 21 cm by 29 cm.'},
     {'filename': 'gallery/owl.jpeg',
-     'title': 'Bowland Hen Harrier. Pencil on paper, 29 cm by 42 cm'},
+     'title': 'Bowland Hen Harrier. Pencil on paper, 29 cm by 42 cm.'},
     {'filename': 'gallery/bluebell.png',
      'title': 'Untitled. Ink, pencil, chalk, watercolour and acrylic.'},
 ]

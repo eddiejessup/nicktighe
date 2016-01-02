470bcd251c581e96aa6c050e43f61d1749aba864
Elliot Marsden <elliot.marsden@gmail.com>
2016-01-02 17:36:10 +0000

    Stop doing silly frozen flask app thing
    The only bit we need is the jinja templating, so just use that.

--- build.py
+++ build.py
@@ -1,52 +1,46 @@
-from __future__ import print_function
-from flask import Flask, render_template
-import locale
-
-locale.setlocale(locale.LC_ALL, locale.getdefaultlocale())
-
-app = Flask(__name__)
-
+from staticjinja import make_site
 
 items = [
-    {'filename': 'gallery/pub.jpeg',
+    {'filename': 'pub.jpeg',
      'title': 'Scarborough Hotel',
      'medium': 'Oil on canvas',
      'size': '44 cm x 62 cm'},
-    {'filename': 'gallery/cow.jpeg',
+    {'filename': 'cow.jpeg',
      'title': 'For the Saward family',
      'medium': 'Oil on canvas',
      'size': '42 cm x 59 cm'},
-    {'filename': 'gallery/dog.jpeg',
+    {'filename': 'dog.jpeg',
      'title': 'Archie',
      'medium': 'Pencil and charcoal on paper',
      'size': '21 cm x 29 cm'},
-    {'filename': 'gallery/owl.jpeg',
+    {'filename': 'owl.jpeg',
      'title': 'Bowland Hen Harrier',
      'medium': 'Pencil on paper',
      'size': '29 cm x 42 cm'},
-    {'filename': 'gallery/he_and_she.jpeg',
+    {'filename': 'he_and_she.jpeg',
      'title': 'He and She',
      'medium': 'Pencil on paper',
      'size': '40 cm x 25 cm'},
-    {'filename': 'gallery/george.jpeg',
+    {'filename': 'george.jpeg',
      'title': 'George the Boxer',
      'medium': 'Pencil, chalk, charcoal, watercolour and acrylic on paper',
      'size': '21 cm x 29 cm'},
-    {'filename': 'gallery/bluebell.jpeg',
+    {'filename': 'bluebell.jpeg',
      'title': 'Untitled',
      'medium': 'Ink, pencil, chalk, watercolour and acrylic on paper'},
 ]
 
 
-@app.route('/index.html')
-def home():
-    return render_template('home.html')
+def get_gallery_context():
+    return {'items': items}
 
 
-@app.route('/gallery.html')
-def gallery():
-    return render_template('gallery.html', items=items)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    site = make_site(
+        contexts=[
+            ('gallery.html', get_gallery_context),
+        ],
+        outpath='public',
+        staticpaths=['static/'],
+    )
+    site.render(use_reloader=False)
